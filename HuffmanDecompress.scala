3aa04cb3fbc67d91a39b3fd5cfebbd59888f8d37
IllegalArgument <awilley1@gmail.com>
2017-06-21 22:08:26 -0400

    Added proper limits/bounds checks to each compressed stream, plus a few new symbols

--- HuffmanDecompress.scala
+++ HuffmanDecompress.scala
@@ -33,49 +33,55 @@ object HuffmanDecompress {
 		val chunkCount = decompressedSize / ChunkSize
 		val headerSize = 0x4 * chunkCount
 		val headerBuffer = ByteBuffer.wrap(fileBytes.take(headerSize)).order(ByteOrder.LITTLE_ENDIAN)
-		// The extra four bytes of padding is needed so that getInt below won't underflow
-		val compressedBuffer = ByteBuffer.wrap(fileBytes.drop(headerSize) ++ Array.ofDim[Byte](4)).order(ByteOrder.BIG_ENDIAN)
+		val compressedBuffer = ByteBuffer.wrap(fileBytes.drop(headerSize)).order(ByteOrder.BIG_ENDIAN)
 		val decompressedBuffer = ByteBuffer.allocate(decompressedSize).order(ByteOrder.BIG_ENDIAN)
 		val chunks = List.fill(chunkCount)({
 			val entry = headerBuffer.getInt()
 			(entry & 0x1FFFFFF, (entry >>> 25) & 0x7F)
 		})
-		chunks.zipWithIndex.foreach({
-			case ((offset, dictionaryType), index) =>
-				println(f"==Processing chunk 0x$index%X at compressed offset 0x$offset%X with dictionary 0x$dictionaryType%X==")
+		chunks.zip(chunks.tail.map(_._1) :+ compressedBuffer.remaining).zipWithIndex.foreach({
+			case (((startOffset, dictionaryType), endOffset), index) =>
+				println(f"==Processing chunk 0x$index%X at compressed offset 0x$startOffset%X with dictionary 0x$dictionaryType%X==")
 				val dictionary = Dictionaries(dictionaryType)
-				compressedBuffer.position(offset)
+				compressedBuffer.position(startOffset).limit(endOffset)
 				decompressedBuffer.position(index * ChunkSize).limit((index + 1) * ChunkSize)
-				var bitBuffer = compressedBuffer.getInt()
-				var availableBits = 32
-				def nextCode(): (Int, Int) = {
-					if (availableBits < codeLength(bitBuffer)) {
-						val next = compressedBuffer.getInt()
-						bitBuffer |= next >>> availableBits
-						val length = codeLength(bitBuffer)
-						val code = (bitBuffer >>> (32 - length)) & 0xFFFF
-						bitBuffer = next << (length - availableBits)
-						availableBits += 32 - length
-						(length, code)
-					} else {
-						val length = codeLength(bitBuffer)
+				var bitBuffer = 0
+				var availableBits = 0
+				while (decompressedBuffer.hasRemaining) {
+					// Fill the bit buffer
+					while (availableBits <= 24 && compressedBuffer.hasRemaining) {
+						bitBuffer |= (compressedBuffer.get() & 0xFF) << (24 - availableBits)
+						availableBits += 8
+					}
+
+					// Calculate the length of the next codeword
+					val length = codeLength(bitBuffer)
+					if (availableBits >= length) {
+						// If we have enough bits buffered, extract the codeword and update the bit buffer
 						val code = (bitBuffer >>> (32 - length)) & 0xFFFF
 						bitBuffer <<= length
 						availableBits -= length
-						(length, code)
-					}
-				}
-				while (decompressedBuffer.hasRemaining) {
-					val (length, code) = nextCode()
-					val symbol = dictionary(length).getOrElse(code, {
-						println(f"Unknown code ${s"%${length}s".format(code.toBinaryString).replace(' ', '0')} (dictionary 0x$dictionaryType%X, length $length, code 0x$code%X) at decompressed offset 0x${decompressedBuffer.position}%X")
-						// 0x7F was arbitrarily chosen as a filler byte
-						Array.fill(1)(0x7F.toByte)
-					})
-					if (decompressedBuffer.remaining >= symbol.length) {
-						decompressedBuffer.put(symbol)
+
+						val symbol = dictionary(length).getOrElse(code, {
+							println(f"Unknown code ${s"%${length}s".format(code.toBinaryString).replace(' ', '0')} (dictionary 0x$dictionaryType%X, length $length, code 0x$code%X) at decompressed offset 0x${decompressedBuffer.position}%X")
+							Array.fill(1)(0x7F.toByte)
+						})
+						if (decompressedBuffer.remaining >= symbol.length) {
+							// If there's stilll space in the decompressed buffer for this chunk, just write the symbol
+							decompressedBuffer.put(symbol)
+						} else {
+							// Although this shouldn't happen with complete dictionaries, since we have a few missing
+							// symbols, it's possible that a codeword is parsed that would map to a symbol that is
+							// longer than the space remaining in the decompressed chunk buffer
+							// If this happens, ignore that last symbol that would otherwise overflow the chunk buffer
+							println(f"Ignoring unaligned final code ${s"%${length}s".format(code.toBinaryString).replace(' ', '0')} (dictionary 0x$dictionaryType%X, length $length, code 0x$code%X) at decompressed offset 0x${decompressedBuffer.position}%X")
+							decompressedBuffer.put(Array.fill(decompressedBuffer.remaining)(0x7F.toByte))
+						}
 					} else {
-						println(f"Ignoring unaligned last code ${s"%${length}s".format(code.toBinaryString).replace(' ', '0')} (dictionary 0x$dictionaryType%X, length $length, code 0x$code%X) at decompressed offset 0x${decompressedBuffer.position}%X")
+						// Since we always try to fill the bit buffer as much as possible before reading the next
+						// codeword, if we read a codeword that requires more bits than we have buffered, we know that
+						// we've reached the end of the compressed stream
+						println(f"Reached end of compressed stream early at decompressed offset 0x${decompressedBuffer.position}%X")
 						decompressedBuffer.put(Array.fill(decompressedBuffer.remaining)(0x7F.toByte))
 					}
 				}
@@ -84,6 +90,7 @@ object HuffmanDecompress {
 	}
 
 	def main(args: Array[String]): Unit = {
+		//TODO: Parse the compressed/decompressed sizes from the metadata file if given as an argument
 		if (args.length == 3) {
 			val compressedFile = Paths.get(args(0))
 			val decompressedFile = Paths.get(args(1))
